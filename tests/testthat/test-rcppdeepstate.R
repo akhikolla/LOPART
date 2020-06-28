ae5ce17352b38b00a2c43fcd73bb5de99db9cabb
akhikolla <akhilakollasrinu424jf@gmail.com>
2020-06-28 15:55:49 -0700

    Test project

--- tests/testthat/test-rcppdeepstate.R
+++ tests/testthat/test-rcppdeepstate.R
@@ -1,14 +1,9 @@
 library(testthat)
-library(RcppDeepState)
+library(testproject)
 library(nc)
 context("rcppdeepstate")
 
-result <- RcppDeepState::check_print()
 
-test_that("valid in lopart", {
-  expect_equal(result,"HI function working")
-  
-})
 print("vars")
 vars <- system.file(package="LOPART")
 print(vars)
@@ -20,12 +15,12 @@ paths <- gsub("/inst","",path)
 print(paths)
 var <- system.file("inst\testfiles",package="LOPART")
 print(var)
-result <- RcppDeepState::package_deepstate_pkg_create("/home/travis/build/akhikolla/LOPART/")
+result <- testproject::package_deepstate_pkg_create("/home/travis/build/akhikolla/LOPART/")
 test_that("deepstate pkg create in lopart", {
   expect_equal(result,"Testharness created!!")
   
 })
 #path <- system.file(package="lopartcheck")
-RcppDeepState::package_deep_harness_compile_run("/home/travis/build/akhikolla/LOPART/")
+testproject::package_deep_harness_compile_run("/home/travis/build/akhikolla/LOPART/")
 
 system("cat /home/travis/build/akhikolla/LOPART/inst/testfiles/LOPART_interface_log")
