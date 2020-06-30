5bd1e47324dd1d34292533a9bae77f4557b6569b
akhikolla <akhilakollasrinu424jf@gmail.com>
2020-06-30 10:06:09 -0700

    check lib

--- tests/testthat/test-rcppdeepstates.R
+++ tests/testthat/test-rcppdeepstates.R
@@ -1,4 +1,13 @@
 library(testthat)
 library(RcppDeepState)
 library(nc)
-context("rcppdeepstate")
+context("rcppdeepstate")package_path <- system.file(package=basename(path))
+result <- RcppDeepState::package_deepstate_pkg_create("package_path")
+test_that("deepstate create TestHarness",{
+  expect_equal(result,"Testharness created!!")
+  })
+RcppDeepState::package_deep_harness_compile_run("package_path")
+error.list <- RcppDeepState::user_error_display("/home/akhila/R/LOPART/inst/testfiles/LOPART_interface_log")
+test_that("log files check",
+{expect_equal(nrows(error.list),0)})
+
