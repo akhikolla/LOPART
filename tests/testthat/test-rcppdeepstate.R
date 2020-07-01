e2d62c46332c24c49ccc640b85830a479fc01902
akhikolla <akhilakollasrinu424jf@gmail.com>
2020-06-30 23:37:16 -0700

    test

--- tests/testthat/test-rcppdeepstate.R
+++ tests/testthat/test-rcppdeepstate.R
@@ -9,13 +9,13 @@ print("paths")
 paths <- gsub("/inst","",path)
 print(paths)
 
-result <- RcppDeepState::package_deepstate_pkg_create("/home/travis/build/akhikolla/LOPART")
+result <- RcppDeepState::deepstate_pkg_create("/home/travis/build/akhikolla/LOPART")
 test_that("deepstate pkg create in lopart", {
   expect_equal(result,"Testharness created!!")
   
 })
 #path <- system.file(package="lopartcheck")
-RcppDeepState::package_deep_harness_compile_run("/home/travis/build/akhikolla/LOPART")
+RcppDeepState::deep_harness_compile_run("/home/travis/build/akhikolla/LOPART")
 result <- user_error_display(system.file("testpkgs/testSAN/inst/testfiles/LOPART_interface_log",package = "RcppDeepState"))
 test_that("deepstate pkg create in lopart", {
 expect_equal(paste0(result$src.file.lines,collapse = ""),"")
