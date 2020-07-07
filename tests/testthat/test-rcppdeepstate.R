61e5354bca7f75d761f4fd5c031ccd4761752883
akhikolla <akhilakollasrinu424jf@gmail.com>
2020-07-07 12:19:15 -0700

    headers check

--- tests/testthat/test-rcppdeepstate.R
+++ tests/testthat/test-rcppdeepstate.R
@@ -4,6 +4,7 @@ library(nc)
 context("rcppdeepstate")
 
 insts_path <- system.file(package="LOPART")
+print(insts_path)
 insts_path <- gsub("/LOPART.Rcheck/LOPART/inst","",insts_path)
 print(insts_path)
 RcppDeepState::make_run()
