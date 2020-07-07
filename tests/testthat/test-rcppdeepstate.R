8b27d1e4274debc69b56044da199200ccf799f9b
akhikolla <akhilakollasrinu424jf@gmail.com>
2020-07-07 13:03:58 -0700

    deepstate ci setup

--- tests/testthat/test-rcppdeepstate.R
+++ tests/testthat/test-rcppdeepstate.R
@@ -11,19 +11,17 @@ RcppDeepState::make_run()
 RcppDeepState::deepstate_ci_setup(insts_path)
 travis_path<-file.path(insts_path,".travis.yml")
 testfile<-"test-rcppdeepstates.R"
-testdir_path<-file.path(path,"tests/testthat/",testfile)
+testdir_path<-file.path(insts_path,"tests/testthat/",testfile)
 test_that("deepstate ci setup test", {
   expect_true(file.exists(travis_path))
   expect_true(file.exists(testdir_path))
 })
 
-package_path <- system.file(package="LOPART")
-package_path <- gsub("/inst","",package_path)
-result <- RcppDeepState::deepstate_pkg_create(package_path)
+result <- RcppDeepState::deepstate_pkg_create(insts_path)
 test_that("deepstate create TestHarness",{
   expect_equal(result,"Testharness created!!")
 })
-RcppDeepState::deep_harness_compile_run(package_path)
+RcppDeepState::deep_harness_compile_run(insts_path)
 error.list <- RcppDeepState::user_error_display(system.file("testfiles/LOPART_interface_log",package="LOPART"))
 test_that("log files check",{
   expect_equal(paste0(error.list$src.file.lines,collapse=""),"")
