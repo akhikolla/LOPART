540a4526038a954b9eaaf50a057870240e8618bb
akhikolla <akhilakollasrinu424jf@gmail.com>
2020-07-07 10:23:20 -0700

    headers check

--- tests/testthat/test-rcppdeepstate.R
+++ tests/testthat/test-rcppdeepstate.R
@@ -3,20 +3,27 @@ library(RcppDeepState)
 library(nc)
 context("rcppdeepstate")
 
-path<-system.file(package="LOPART")
-# print(path)
-path <- gsub("/inst","",path)
-RcppDeepState::deepstate_create_static_lib()
-result <- RcppDeepState::deepstate_pkg_create(path)#"/home/travis/build/akhikolla/LOPART")
-test_that("deepstate pkg create in lopart", {
-  expect_equal(result,"Testharness created!!")
-  
+insts_path <- system.file(package="LOPART")
+insts_path <- gsub("/inst","",insts_path)
+print(insts_path)
+RcppDeepState::make_run()
+RcppDeepState::deepstate_ci_setup(insts_path)
+travis_path<-file.path(insts_path,".travis.yml")
+testfile<-"test-rcppdeepstates.R"
+testdir_path<-file.path(path,"tests/testthat/",testfile)
+test_that("deepstate ci setup test", {
+  expect_true(file.exists(travis_path))
+  expect_true(file.exists(testdir_path))
 })
-#path <- system.file(package="lopartcheck")
-RcppDeepState::deep_harness_compile_run(path)#"/home/travis/build/akhikolla/LOPART")
-#system("cat /home/travis/build/akhikolla/LOPART/inst/testfiles/LOPART_interface_log")
-result <- user_error_display(system.file("testfiles/LOPART_interface_log",package="LOPART"))
-#"/home/travis/build/akhikolla/LOPART/inst/testfiles/LOPART_interface_log")
-test_that("deepstate pkg create in lopart", {
-expect_equal(paste0(result$src.file.lines,collapse = ""),"")
+
+package_path <- system.file(package="LOPART")
+package_path <- gsub("/inst","",package_path)
+result <- RcppDeepState::deepstate_pkg_create(package_path)
+test_that("deepstate create TestHarness",{
+  expect_equal(result,"Testharness created!!")
+})
+RcppDeepState::deep_harness_compile_run(package_path)
+error.list <- RcppDeepState::user_error_display(system.file("testfiles/LOPART_interface_log",package="LOPART"))
+test_that("log files check",{
+  expect_equal(paste0(error.list$src.file.lines,collapse=""),"")
 })
